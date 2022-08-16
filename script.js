5d2d6b8b809ff36924ad86363049c85ca306943e
Aunirbhan Das <aunirbhandas@Aunirbhans-Air.lan>
2022-08-16 10:27:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,101 @@
+let mainHeader = document.getElementById("mainHeader");
+let mainHeaderContainer = document.getElementById("mainHeaderContainer");
+let mainPageImg = document.getElementById("mainPageImg")
+let input = document.getElementById("input")
+let button = document.getElementById("button")
+let recipeContainer = document.getElementById("recipeContainer")
+
+// these funtions are style choices to make the main heading on each page stand out through fading on scroll and enlarging on hover
+mainHeader.onmouseover = function(){
+    mouseOver();
+}
+
+mainHeader.onmouseout = function(){
+    mouseOut();
+}
+
+function mouseOver() {
+    mainHeader.style.fontSize = "350%";
+    mainHeader.style.color = "#0088a9";
+}
+  
+function mouseOut() {
+    mainHeader.style.fontSize = "275%";
+    mainHeader.style.color = "#283747";
+}
+
+function fadeOutOnScroll(element) {
+    if (!element) {
+      return;
+    }
+    
+    let distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+    let elementHeight = element.offsetHeight;
+    let scrollTop = document.documentElement.scrollTop;
+    
+    let opacity = 1;
+    
+    if (scrollTop > distanceToTop) {
+      opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+    }
+    
+    if (opacity >= 0) {
+      element.style.opacity = opacity;
+    }
+  }
+  
+function scrollHandler() {
+    fadeOutOnScroll(mainHeaderContainer);
+  }
+  
+window.addEventListener('scroll', scrollHandler);
+
+
+input.addEventListener("keypress", function(event){
+    if(event.key === "Enter"){
+        event.preventDefault();
+        document.getElementById("button").click();
+    }
+});
+
+
+button.onclick = function(event){
+  event.preventDefault()
+  let input = document.querySelector("input").value
+  fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${input}&addRecipeInformation=true&apiKey=cbe7394d07c9409dbad6ca4be089171c`)
+      .then(function(response) {
+          return response.json();
+      })
+      .then(function(data){
+        recipeContainer.innerHTML = "";
+        let item = data.results;
+        console.log(item)
+        for(let i=0;i<item.length;i++){
+          console.log(item[i])
+          let newDiv = document.createElement("div")
+
+            //imageSrc
+            let foodImage = document.createElement("img")
+            foodImage.src = item[i].image;
+            
+            //title
+            let foodTitle = document.createElement("h3")
+            foodTitle.innerHTML = item[i].title
+
+            //cuisine
+            // let  = document.createElement("p")
+            // foodCuisine.innerHTML = item[i].cuisine
+            
+            //ready
+            let foodReady = document.createElement("p")
+            foodReady.innerHTML = "Ready within "+item[i].readyInMinutes+" minutes"
+            
+            //appending the data 
+            newDiv.append(foodTitle, foodReady, foodImage)
+            console.log(newDiv)
+            console.log(recipeContainer)
+            recipeContainer.append(newDiv)
+            console.log(recipeContainer)
+        }
+      })
+}
