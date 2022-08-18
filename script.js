577375b13c7c651b36e650aced666f32bf79898d
Aunirbhan Das <aunirbhandas@Aunirbhans-Air.lan>
2022-08-18 13:38:23 -0400

    adding images and removing fadeout

--- script.js
+++ script.js
@@ -23,29 +23,29 @@ function mouseOut() {
     mainHeader.style.color = "#283747";
 }
 
-function fadeOutOnScroll(element) {
-    if (!element) {
-      return;
-    }
+// function fadeOutOnScroll(element) {
+//     if (!element) {
+//       return;
+//     }
     
-    let distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-    let elementHeight = element.offsetHeight;
-    let scrollTop = document.documentElement.scrollTop;
+//     let distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+//     let elementHeight = element.offsetHeight;
+//     let scrollTop = document.documentElement.scrollTop;
     
-    let opacity = 1;
+//     let opacity = 1;
     
-    if (scrollTop > distanceToTop) {
-      opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
-    }
+//     if (scrollTop > distanceToTop) {
+//       opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+//     }
     
-    if (opacity >= 0) {
-      element.style.opacity = opacity;
-    }
-  }
-function scrollHandler() {
-    fadeOutOnScroll(mainHeaderContainer);
-  }
-window.addEventListener('scroll', scrollHandler);
+//     if (opacity >= 0) {
+//       element.style.opacity = opacity;
+//     }
+//   }
+// function scrollHandler() {
+//     fadeOutOnScroll(mainHeaderContainer);
+//   }
+// window.addEventListener('scroll', scrollHandler);
 
 
 input.addEventListener("keypress", function(event){
